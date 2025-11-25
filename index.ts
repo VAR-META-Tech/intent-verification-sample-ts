2fd75de38547b530ea18cbe86d47c5f7e9817265
ngoc <ngochc1@gmail.com>
2025-11-25 17:05:05 +0700

    Update test

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// Intentionally wrong sum function (adds 1 extra)
 export function sum(a: number, b: number): number {
-  return a + b + 1; // Wrong! Should be just a + b
+  return a + b + 1; 
 }
-
-console.log("Hello via Bun!");
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,6 +3,6 @@ import { sum } from "..";
 
 describe("sum", () => {
   test("adds 1 + 2 to equal 2", () => {
-    expect(sum(1, 2)).toBe(2);
+    expect(sum(1, 2)).toBe(3);
   });
 });
